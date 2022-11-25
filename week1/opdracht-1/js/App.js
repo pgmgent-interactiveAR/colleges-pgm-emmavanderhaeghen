04afbfa6964ad5d2b19bd9153733ad6eedf62d38
pgm-emmavanderhaeghen <emmavand24@student.arteveldehs.be>
2022-11-25 13:57:58 +0100

    opdracht

---
+++ week1/opdracht-1/js/App.js
@@ -0,0 +1,12 @@
+// registering components
+class App {
+    constructor() {
+        this.registerComponents();
+    }
+    registerComponents() {
+        this.runButton = document.querySelector("#btnRun")
+    }
+    registerEventListeners() {
+      this.runButton
+    }
+}
